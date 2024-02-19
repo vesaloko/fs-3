aaba9a9624250cf7b4432107b61410850f28c292
Salokoski Venla Josefina <ven.lasalokoski@gmail.com>
2024-02-19 15:03:23 +0200

    modules

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ app.use(express.json())
 //app.use(requestLogger)
 app.use(morgan('tiny'))
 
+const cors = require('cors')
+app.use(cors())
+
 morgan.token('post-info', function getData(request, response)
                 {
                     let person = {
@@ -132,7 +135,7 @@ let persons =[
       app.use(unknownEndpoint)
 
 
-      const PORT = 3001
+      const PORT = process.env.PORT || 3001
       app.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`)
       })
