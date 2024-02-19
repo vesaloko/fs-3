8237afedecd764141c21b67564cdf1c738d75b17
Salokoski Venla Josefina <ven.lasalokoski@gmail.com>
2024-02-19 17:27:37 +0200

    toimii

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const morgan = require('morgan')
 app.use(bodyParser.json())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.static('dist'))
 
 const cors = require('cors')
 app.use(cors())
