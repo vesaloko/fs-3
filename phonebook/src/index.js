9dc4c7ca0c6de46a00699479bd340d1304d0fc4c
Salokoski Venla Josefina <ven.lasalokoski@gmail.com>
2024-02-19 14:29:42 +0200

    frontend

---
+++ phonebook/src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom'
+import App from './App'
+
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+)
+
+///phonebook osittain otettu teht 2 mallivastauksesta 
